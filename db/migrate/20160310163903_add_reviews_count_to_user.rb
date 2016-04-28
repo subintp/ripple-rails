116dfbe599d54a69991e48b2677639b143804aea
subintp <subin.tp@chillr.in>
2016-04-28 23:03:07 +0530

    Adds default value for reviews count

--- db/migrate/20160310163903_add_reviews_count_to_user.rb
+++ db/migrate/20160310163903_add_reviews_count_to_user.rb
@@ -1,5 +1,5 @@
 class AddReviewsCountToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :reviews_count, :integer
+    add_column :users, :reviews_count, :integer, default: 0
   end
 end
