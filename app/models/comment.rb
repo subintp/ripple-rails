1737473823e9a8a6a2f6f98ce10494ce2bcb5c58
subintp <subin.tp@chillr.in>
2016-05-17 01:58:39 +0530

    Comment created activity

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,23 @@ class Comment < ApplicationRecord
   acts_as_mentioner
 
   validates :content, presence: true
+
+  include PublicActivity::Model
+  tracked
+
+  after_create :create_activities
+
+  private
+
+  def create_activities
+    self.create_activity(key: "review.comment",
+                    owner: User.current_user,
+                    parameters: { owner_name: User.current_user.name,
+                                  recipent_name: review.user.name,
+                                  target_name: review.movie.name,
+                                   target_pic: User.current_user.avathar_url
+                                 })
+
+  end
+
 end
