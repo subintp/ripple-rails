4395f735a1d8948bbec4e8cb1a39231acbdb2c8b
subintp <subin.tp@chillr.in>
2016-05-23 19:39:53 +0530

    Adds activity for review and like

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,15 @@
 class Follow < Socialization::ActiveRecordStores::Follow
   include PublicActivity::Model
   tracked
+
+  after_create :create_activities
+
+  private
+
+  def create_activities
+    PublicActivity.enabled = true
+    create_activity key: "user.follow", owner: User.current_user, parameters:  {owner_name: User.current_user.name, target_name: followable.name, target_pic: followable.avathar_url }
+    PublicActivity.enabled = false
+  end
+
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,14 @@
 class Like < Socialization::ActiveRecordStores::Like
   include PublicActivity::Model
   tracked
+
+  after_create :create_activities
+
+  private
+
+  def create_activities
+    PublicActivity.enabled = true
+    create_activity key: "review.like", owner: User.current_user, parameters: {owner_name: User.current_user.name, recipent_name: likeable.user.name, target_name: likeable.user.name, target_pic: User.current_user.avathar_url }
+    PublicActivity.enabled = false
+  end
 end
